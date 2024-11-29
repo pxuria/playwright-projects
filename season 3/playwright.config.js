50dac3f12bc32719cb8c6c3e5c3b75d57a6e731e
pxuria <pouriamesram@gmail.com>
2024-11-29 23:39:43 +0330

    create locators

--- season 3/playwright.config.js
+++ season 3/playwright.config.js
@@ -4,6 +4,12 @@ const { defineConfig, devices } = require('@playwright/test');
 module.exports = defineConfig({
   testDir: './tests',
   reporter: 'html',
+  // maximum time for test
+  timeout: 30 * 1000,
+  expect: {
+    // maximum time for expect
+    timeout: 7000
+  },
   use: {
     browserName: "chromium",
     headless: true
--- season 3/tests/example.spec.js
+++ season 3/tests/example.spec.js
@@ -1,7 +1,23 @@
 import { test, expect } from "@playwright/test"
 
+// season 3
 test("google first test", async ({ page }) => {
   await page.goto("https://google.com")
-  // await expect(page.title)
+  await expect(page).toHaveTitle("Google")
   console.log(await page.title())
+})
+
+
+// season 4
+test.only("rahulshettyacademy first test", async ({ page }) => {
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise")
+  console.log(await page.title());
+
+  // selectors [css , xpath]
+  // filling
+  await page.locator("#username").fill("rahulshettyacademy ");
+  await page.locator("[type='password']").fill("learning");
+
+  // clicking
+  await page.locator("#signInBtn").click();
 })
